e9a48efcbefb030b5640ff4bcf9acf7d83d2e468
Swiftynoofty <ggorski2022@elam.co.uk>
2022-09-13 12:02:17 +0100

    Runner

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,10 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject groundChecker;
     public LayerMask whatIsGround;
-
-
-    float maxSpeed = 5.0f;
+    public float jumpForce;
+    public float tempSpeed = 10.0f;
+    public float maxSpeed = 5.0f;
     bool isOnGround = false;
+    bool issprint = false;
+
+    
 
     Rigidbody2D playerObject;
     // Start is called before the first frame update
@@ -23,12 +26,44 @@ public class PlayerController : MonoBehaviour
     {
         float movementValueX = Input.GetAxis("Horizontal");
 
-        playerObject.velocity = new Vector2 (movementValueX * maxSpeed, playerObject.velocity.y);
-        isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatIsGround);
+        playerObject.velocity = new Vector2 (movementValueX * maxSpeed, playerObject.velocity.y);//running/walking
+        isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatIsGround);//jumpping
+        
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+            issprint = true;
+
+        }
+        else
+        {
+           issprint = false;
+        }
+        
+       
+      
+
 
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
         {
-            playerObject.AddForce(new Vector2(0.0f, 100.0f));
+            playerObject.AddForce(new Vector2(0.0f, jumpForce));
         }
     }
+    void LateUpdate()
+    {
+        if (issprint == false)
+        {
+            maxSpeed = 5f;  
+
+        } 
+        if (issprint == true)
+        {
+            maxSpeed = tempSpeed;
+        
+        }  
+
+
+    }
+
+    
 }
